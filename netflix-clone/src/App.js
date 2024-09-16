f0aff29fdf8c567a5be40b1436d9ab2daf90a642
SemiraAlewi <132842438+SemiraAlewi@users.noreply.github.com>
2024-09-16 16:08:42 +0300

    header and footer

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Home from './pages/home/Home';
+import './index.css';
+
+function App() {
+  return (
+    <div >
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ netflix-clone/src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../component/header/Header';
+import Footer from '../../component/footer/Footer';
+
+function Home() {
+  return (
+    <>
+<Header/>
+<Footer/>
+    </>
+  );
+}
+
+export default Home;
