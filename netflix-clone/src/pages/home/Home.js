a544563d62832fba4466593c6002d400ddac9ada
SemiraAlewi <132842438+SemiraAlewi@users.noreply.github.com>
2024-09-16 20:49:00 +0300

    all project

--- netflix-clone/src/pages/home/Home.js
+++ netflix-clone/src/pages/home/Home.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Header from '../../component/header/Header';
 import Footer from '../../component/footer/Footer';
+import Banner from '../../component/Banner/Banner';
+import RowList from '../../component/Rows/RowList/RowList';
+import Row from '../../component/Rows/Row/Row';
 
 function Home() {
   return (
     <>
 <Header/>
+<Banner/>
+<Row/>
+<RowList/>
 <Footer/>
+
+
     </>
   );
 }
